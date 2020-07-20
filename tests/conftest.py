9aefb4f8861b507427061a2334d6012f7d0f58c3
Fisher Ankney <fisher.ankney@capspire.com>
2020-07-20 15:46:55 -0600

    stuck with rebuild

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,24 @@
-import os
 
+import os
+import sys
 import pytest
 from starlette.testclient import TestClient
 
-from app import main
+sys.path.append('/project')
+
+from app.main import create_application
 from app.config import get_settings, Settings
 
 def get_settings_override():
     return Settings(testing=1, database_url=os.environ.get("DATABASE_TEST_URL"))
 
+
 @pytest.fixture(scope="module")
 def test_app():
     # set up
-    main.app.dependency_overrides[get_settings] = get_settings_override
-    with TestClient(main.app) as test_client:
+    app = create_application()  # new
+    app.dependency_overrides[get_settings] = get_settings_override
+    with TestClient(app) as test_client:  # updated
 
         # testing
         yield test_client
